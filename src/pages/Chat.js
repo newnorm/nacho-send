d9dbb8e184cdef2e54e0a785589bef2d01f6f640
suyeon woo <suyeon.woo@grotesq.com>
2020-11-20 13:27:10 +0900

    커스텀 훅으로 react material ui 에서 사용할  폰트 추가

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import App from './App'
 //TODO: material ui inputs
 
 const Chat = (props) => {
   return (
-    <App>
+    <>
       <div>chat room</div>
-    </App>
+    </>
   )
 }
 
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import {Home, About} from "../pages";
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { Home, About, Chat } from '../pages'
+import useScript from '../hooks/useScript'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-      </div>
-    );
-  }
+function App(props) {
+  //커스텀 훅 usescript,,, 잘 동작할까?
+  // next js 처럼 최상단 Document에 위치하게 하고 싶은데 이렇게 하면 되는지 ..?
+  // https://qastack.kr/programming/34424845/adding-script-tag-to-react-jsx
+
+
+  useScript(
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+  )
+
+  return (
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/chat" component={Chat} />
+    </div>
+  )
 }
 
-export default App;
+export default App

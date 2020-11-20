418627cfe26c3fc09aa1cf538be138a0e3cfc239
suyeon woo <suyeon.woo@grotesq.com>
2020-11-20 13:59:05 +0900

    public 디렉토리에 폰트 및 아이콘 삽입

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,22 @@
 import React from 'react'
 //TODO: material ui inputs
+import styled from 'styled-components'
+import useScript from '../hooks/useScript'
+import Button from '@material-ui/core/Button'
+
+const StyledFont = styled.p`
+  font-family: sans-serif;
+`
 
 const Chat = (props) => {
   return (
     <>
-      <div>chat room</div>
+      <p>chat room</p>
+      <StyledFont>chat room</StyledFont>
+      {/*TODO: roboto 폰트 적용*/}
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </>
   )
 }

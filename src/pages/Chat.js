95fb4cb2978501a07ad377628c036483dc1c95be
suyeon woo <suyeon.woo@grotesq.com>
2020-11-20 14:05:21 +0900

    기본 input ui 리액트머티리얼 ui 로 제작

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -3,21 +3,38 @@ import React from 'react'
 import styled from 'styled-components'
 import useScript from '../hooks/useScript'
 import Button from '@material-ui/core/Button'
+import Input from '@material-ui/core/Input'
 
 const StyledFont = styled.p`
   font-family: sans-serif;
 `
 
+const Layout = styled.div`
+  padding-top: 100px;
+  margin: 0 auto;
+  width: 80%;
+`
+
 const Chat = (props) => {
+  const [typing, setTyping] = React.useState('')
   return (
-    <>
+    <Layout>
       <p>chat room</p>
       <StyledFont>chat room</StyledFont>
       {/*TODO: roboto 폰트 적용*/}
+      <Input
+        style={{ width: 500, marginRight: 10 }}
+        placeholder={'입력해주세요...'}
+        value={typing}
+        onChange={(event) => {
+          setTyping(event.target.value)
+        }}
+      />
+
       <Button variant="contained" color="primary">
-        Hello World
+        submit
       </Button>
-    </>
+    </Layout>
   )
 }
 

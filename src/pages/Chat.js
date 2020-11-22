802357af623aec8d455eb667d0899bf5c67ef752
suyeon woo <suyeon.woo@grotesq.com>
2020-11-22 16:31:58 +0900

    socket io 붙임

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,22 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 //TODO: material ui inputs
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button'
-import Input from '@material-ui/core/Input'
 import produce from 'immer'
-import {
-  fade,
-  ThemeProvider,
-  withStyles,
-  makeStyles,
-  createMuiTheme,
-} from '@material-ui/core/styles'
-
-import InputBase from '@material-ui/core/InputBase'
-import InputLabel from '@material-ui/core/InputLabel'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
-import FormControl from '@material-ui/core/FormControl'
-import { green } from '@material-ui/core/colors'
+import io from 'socket.io-client'
 
 const CssTextField = withStyles({
   root: {
@@ -29,23 +18,13 @@ const CssTextField = withStyles({
   },
 })(TextField)
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-}))
-
 const Layout = styled.div`
   padding-top: 100px;
   margin: 0 auto;
   width: 50%;
 `
 
-const InputWrapper = styled.p`
+const InputWrapper = styled.div`
   margin-bottom: 20px;
   width: 100%;
   display: flex;
@@ -53,40 +32,47 @@ const InputWrapper = styled.p`
 `
 
 const Chat = (props) => {
-  const [typer, setTyper] = React.useState('newnorm')
-  const [typing, setTyping] = React.useState('')
-  const [contents, setContents] = React.useState([])
-  const contentsArray = []
+  const [yourId, setYourId] = React.useState('')
+  const [messages, setMessages] = React.useState('')
+  const [message, setMessage] = React.useState('')
 
-  const classes = useStyles()
+  const socketRef = useRef()
 
   React.useEffect(() => {
-    // setContents(contentsArray)
-    // contentsArray.push(typing)
-    setTyping('')
-  }, [contents])
+    socketRef.current = io.connect('/')
+
+    socketRef.current.on('your id', (id) => {
+      setYourId(id)
+    })
+
+    socketRef.current.on('message', (message) => {
+      console.log(message)
+      setMessages((oldMsgs) => [...oldMsgs, message])
+    })
+  }, [])
 
   return (
     <Layout>
       <h1 style={{ color: '#92cd28' }}>Nacho Chat</h1>
-      {/*<StyledFont>chat room</StyledFont>*/}
       <InputWrapper>
         <CssTextField
           id="custom-css-standard-input"
-          label={`${typer} 's saying...`}
+          label={`${yourId} 's saying...`}
           style={{ width: 800, marginRight: 10, marginBottom: 20 }}
           placeholder={'입력해주세요...'}
-          value={typing}
+          value={message}
           onChange={(event) => {
-            setTyping(event.target.value)
+            setMessage(event.target.value)
           }}
           onKeyDown={(event) => {
-            if (event.key === 'Enter' && typing) {
-              setContents(
-                produce(contents, (draft) => {
-                  draft.push(typing)
-                })
-              )
+            if (event.key === 'Enter' && message) {
+              const messageObject = {
+                body: message,
+                id: yourId,
+              }
+
+              setMessage('')
+              socketRef.current.emit('send message', messageObject)
             }
           }}
         />
@@ -95,35 +81,41 @@ const Chat = (props) => {
           variant="contained"
           style={{ backgroundColor: '#e4ff7f', height: 40 }}
           onClick={() => {
-            // setContents(contentsArray)
-            // contentsArray.push(typing)
-            if (typing) {
-              setContents(
-                produce(contents, (draft) => {
-                  draft.push(typing)
-                })
-              )
+            const messageObject = {
+              body: message,
+              id: yourId,
             }
+            setMessage('')
+            socketRef.current.emit('send message', messageObject)
           }}
         >
           send
         </Button>
       </InputWrapper>
 
-      {contents
-        .slice(0)
-        .reverse()
-        .map((content, index) => {
-          //원본은 바꾸지 않고 얕은 복사 한 후 reverse 후 map
-          return (
-            <>
-              <div style={{ color: '#f78914' }} key={index.toString()}>
-                {typer} : {content}
+      {messages &&
+        messages
+          ?.slice(0)
+          .reverse()
+          .map((message, index) => {
+            //원본은 바꾸지 않고 얕은 복사 한 후 reverse 후 map
+
+            if (message.id === yourId) {
+              return (
+                <div key={index.toString()}>
+                  <div style={{ color: '#f78914' }}>{message.body}</div>
+                  <br />
+                </div>
+              )
+            }
+
+            return (
+              <div key={index.toString()}>
+                <div style={{ color: '#92cd28' }}>{message.body}</div>
+                <br />
               </div>
-              <br />
-            </>
-          )
-        })}
+            )
+          })}
     </Layout>
   )
 }
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const http = require('http')
 const app = express()
-const api = require('./routes')
-const cors = require('cors')
-app.use(cors())
-app.use('/api', api)
+const server = http.createServer(app)
+const socket = require('socket.io')
+const io = socket(server)
+// const api = require('./routes')
+// const cors = require('cors')
+// app.use(cors())
+// app.use('/api', api)
 
-const port = 3001
-app.listen(port, ()=>{console.log(`listening on port ${port}`)})
+const port = 8000
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`)
+// })
+
+io.on('connection', (socket) => {
+  socket.emit('your id', socket.id)
+  socket.on('send message', (body) => {
+    io.emit('message', body)
+  })
+})
+//접속 아이디를 알려주고, 메시지를 보낸다
+
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+})

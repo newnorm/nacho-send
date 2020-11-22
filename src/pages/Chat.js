4da25260a7f0a9b04ca1fde526867a62c6f9cd8a
suyeon woo <suyeon.woo@grotesq.com>
2020-11-22 13:04:30 +0900

    채팅 인풋에 채워진 값을 내용에 배열로 붙이기

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import useScript from '../hooks/useScript'
 import Button from '@material-ui/core/Button'
 import Input from '@material-ui/core/Input'
+import produce from 'immer'
 
 const StyledFont = styled.p`
   font-family: sans-serif;
@@ -17,10 +18,19 @@ const Layout = styled.div`
 
 const Chat = (props) => {
   const [typing, setTyping] = React.useState('')
+  const [contents, setContents] = React.useState([])
+  const contentsArray = []
+
+  React.useEffect(() => {
+    // setContents(contentsArray)
+    // contentsArray.push(typing)
+    setTyping('')
+  }, [contents])
+
   return (
     <Layout>
-      <p>chat room</p>
-      <StyledFont>chat room</StyledFont>
+      <h1>chat room</h1>
+      {/*<StyledFont>chat room</StyledFont>*/}
       {/*TODO: roboto 폰트 적용*/}
       <Input
         style={{ width: 500, marginRight: 10 }}
@@ -31,9 +41,29 @@ const Chat = (props) => {
         }}
       />
 
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          // setContents(contentsArray)
+          // contentsArray.push(typing)
+          setContents(
+            produce(contents, (draft) => {
+              draft.push(typing)
+            })
+          )
+        }}
+      >
         submit
       </Button>
+      {contents.map((content, index) => {
+        return (
+          <>
+            <div key={index.toString()}>{content}</div>
+            <br />
+          </>
+        )
+      })}
     </Layout>
   )
 }
--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const api = require('./routes')
 const cors = require('cors')
-
 app.use(cors())
 app.use('/api', api)
 
-const port = 300
+const port = 3001
 app.listen(port, ()=>{console.log(`listening on port ${port}`)})
